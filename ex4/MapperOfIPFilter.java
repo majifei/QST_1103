76ec39c3561f2db0ad708265877910c3ec1066b2
Leo <msm1456@126.com>
2016-11-09 20:24:47 +0800

    Update MapperOfIPFilter.java

--- ex4/MapperOfIPFilter.java
+++ ex4/MapperOfIPFilter.java
@@ -3,10 +3,19 @@ package No4;
 import java.util.Scanner;
 
 public class MapperOfIPFilter {
-	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()){
-			String line = scanner.nextLine();
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		while (s.hasNext())
+
+		{
+			String line = s.nextLine();
+			String[] tt;
+			tt = line.split(" ");// 切
+
+			String str = tt[3];// 去[
+
+			System.out.printf("%s\t%s\n", tt[0], str.substring(1, str.length()));// 截取第一个到.length长度个字符
+
 		}
 	}
 }
