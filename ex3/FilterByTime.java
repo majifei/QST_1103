827f984b1a5b4e48619852dcbb3f5dcd9ca40bac
Leo <msm1456@126.com>
2016-11-09 17:01:45 +0800

    Update FilterByTime.java

--- ex3/FilterByTime.java
+++ ex3/FilterByTime.java
@@ -6,6 +6,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * 题目要求：
@@ -16,23 +18,38 @@ import java.util.Scanner;
  * 2. 提交代码到分支下，创建pull request，与主仓库的master分支对比
  */
 public class FilterByTime {
-	
-	public static void main(String[] args) throws ParseException, FileNotFoundException{
+
+	public static void main(String[] args) throws ParseException, FileNotFoundException {
 		SimpleDateFormat regularFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date beginDate = regularFormat.parse("2015-12-31 18:00:00");
 		Date endDate = regularFormat.parse("2015-12-31 19:00:00");
 		String filePath = "./access.log";
 		FileInputStream inputStream = new FileInputStream(filePath);
 		Scanner scanner = new Scanner(inputStream, "UTF-8");
-		while (scanner.hasNext()){
+		while (scanner.hasNext()) {
 			// 对每行进行处理
 			String line = scanner.nextLine();
 			// 切分获取IP，Time
-			String strIp = null;
-			String strTime = null;
+
+			Pattern time = Pattern.compile(
+					"(((0[1-9]|[12][0-9]|3[01])/((0[13578]|1[02]))|((0[1-9]|[12][0-9]|30)/(0[469]|11))|(0[1-9]|[1][0-9]|2[0-8])/(02))/([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3}))|(29/02/(([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00)))");
+			Matcher mTime = time.matcher(line);
+
+			
+
+			String strTime = mTime.group(1);
+
+			String[] strIp = line.split(" ");
+
+			Date date = regularFormat.parse(strTime);
 			// 对在时间区间内的数据进行输出
-			System.out.println(strIp + "\t" + strTime);
+			if(endDate.getTime() > date.getTime() && date.getTime() > beginDate.getTime()) {
+
+				System.out.println(strIp[0] + "\t" + date);
+
+			}
+
 		}
 	}
-	
+
 }
