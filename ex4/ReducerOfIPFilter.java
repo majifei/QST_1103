67c61c9bd39107576e03f159895cb91dff9cd384
Leo <msm1456@126.com>
2016-11-09 20:27:52 +0800

    Update ReducerOfIPFilter.java

--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -1,12 +1,40 @@
 package No4;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class ReducerOfIPFilter {
-	public static void main(String[] args){
+	public static void main(String[] args) throws ParseException {
+
+		String key;
+		String value;
+		Set<String> set = new HashSet<String>();
+		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()){
+		SimpleDateFormat regularFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss");
+		// 输入？
+		Date beginDate = regularFormat.parse("30/Dec/2015:00:02:09");
+		Date endDate = regularFormat.parse("31/Dec/2015:00:02:09");
+
+		while (scanner.hasNext()) {
 			String line = scanner.nextLine();
+			String[] val = line.split("\t");
+			key = val[1];// time
+			value = val[0];// ip
+			Date date = regularFormat.parse(key);// strong转data
+			if (endDate.getTime() > date.getTime() && date.getTime() > beginDate.getTime()) {
+				// ip去重
+
+				set.add(value);
+
+			}
+
 		}
+
+		System.out.println(set.size());
 	}
 }
